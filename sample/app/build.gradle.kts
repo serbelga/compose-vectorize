d229f784fdd0dd859ba8d928148e522c9a8e24d9
Sergio Belda Galbis <sbeldagalbis@gmail.com>
2024-04-08 17:59:20 +0200

    Add Paparazzi

--- sample/app/build.gradle.kts
+++ sample/app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("dev.sergiobelda.compose.vectorize")
     id("dev.sergiobelda.gradle.spotless")
+    alias(libs.plugins.paparazzi)
 }
 
 group = "dev.sergiobelda.compose.vectorize"
